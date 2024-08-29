403cd3ebed693d552daf0d4843970fb88c4192d0
nftmoondogs <nftmoondogs@gmail.com>
2024-08-29 12:04:09 +0530

    newcommit

--- src/components/TokenomicsSection.js
+++ src/components/TokenomicsSection.js
@@ -18,12 +18,12 @@ const TokenomicsSection = () => {
       <div className="container mx-auto grid md:grid-cols-2 gap-8 items-center">
         {/* Content Section */}
         <div>
-        <h2 className="text-3xl font-bold mb-6 flashy-text">No more Mr. Nice Bean. This is War, uncensored and unfiltered. "You think you can crush me?" $BEAN taunts, "Try me. Try stopping me before I completes the Bonding Curve on pump.sol and gets sent outside these walls into my FREEDOM Become token on Solana and burn all jeets down. You're all free, huh? Free to run, free to hide, 'cause I'm coming, and I won't stop until every last jeeter is dust under SSmy roots."
+        <h2 className="text-3xl font-bold mb-6 flashy-text">No more Mr. Nice Bean. This is War, uncensored and unfiltered. "You think you can crush me?" $BEAN taunts, "Try me. Try stopping me before I completes the Bonding Curve on SunPump and gets sent outside these walls into my FREEDOM Become token on Tron and burn all jeets down. You're all free, huh? Free to run, free to hide, 'cause I'm coming, and I won't stop until every last jeeter is dust under SSmy roots."
 
 </h2>
 
           <p className="mb-4">
-            $BEAN PreSale will take place on Pump.Fun💊, BEAN WILL MAKE JEETS CRY
+            $BEAN PreSale will take place on SunPump.meme🌞, BEAN WILL MAKE JEETS CRY
           </p>
           <h3 className="text-2xl font-bold mb-4 flashy-text">Radium is FREEDOM???</h3>
           <ul className="list-disc list-inside space-y-2">
