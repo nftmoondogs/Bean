88fcb9054f5e90ec03b10a7b7bd20455161d1d42
nftmoondogs <nftmoondogs@gmail.com>
2024-10-09 19:15:18 +0530

    newcommit

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -18,7 +18,7 @@ export default function AboutSection() {
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
         {/* Left Side Content */}
         <p className="font-courier text-6xl mb-8">BEAN TOKEN launching on MoonShot🌙</p>
-        <a href="https://dexscreener.com/solana/5hVJqSbb46wm7wscir1mMFDmXBadvoKUwVE1hZXSELWE" target="_blank">
+        <a href="https://dexscreener.com/solana/" target="_blank">
   <button type="button" class="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Buy on MoonShot🌙</button>
 </a>
         <div className="font-stone"> {/* Apply the font to the entire div to affect all child text elements */}
